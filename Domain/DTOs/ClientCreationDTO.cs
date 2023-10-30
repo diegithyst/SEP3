4fd0969825922ef90a16ec0cb25a10d1eeba0725
00FA12 <114292738+00FA12@users.noreply.github.com>
2023-10-30 10:45:18 +0100

    Birthday now as a string

--- Domain/DTOs/ClientCreationDTO.cs
+++ Domain/DTOs/ClientCreationDTO.cs
@@ -5,10 +5,10 @@ public class ClientCreationDTO
     public string name { get; set; }
     public string country { get; set; }
     public string id { get; set; }
-    public DateTime birthday { get; set; }
+    public string birthday { get; set; }
     public string planType { get; set; }
 
-    public ClientCreationDTO(string name, string country, string id, DateTime birthday, string planType)
+    public ClientCreationDTO(string name, string country, string id, string birthday, string planType)
     {
         this.name = name;
         this.country = country;
--- Domain/Model/Client.cs
+++ Domain/Model/Client.cs
@@ -5,7 +5,7 @@ public class Client
     public string name { get; set; }
     public string country { get; set; }
     public string identityDocument { get; set; }
-    public DateTime birthday { get; set; }
+    public string birthday { get; set; }
     public string planType { get; set; }
     private int id { get; set; }
 }
