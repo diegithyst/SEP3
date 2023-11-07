a3b3816dd117004a959b4ccc737afb653f90a7a7
HCDani <hockdanield@gmail.com>
2023-11-07 13:07:46 +0100

    Client has been refactored

--- PersistentServer/src/main/java/org/via/sep3/persistentserver/Main.java
+++ PersistentServer/src/main/java/org/via/sep3/persistentserver/Main.java
@@ -79,7 +79,7 @@ public class Main {
                 if (s.createQuery("from Client", Client.class).stream().count()<1){
                     Transaction t = s.beginTransaction();
                     MyLogger.getInstance().log("***psinit","db is empty so put some bootstrap data into it. ");
-                    s.persist(new Client("en","hungary","AAAAA","31042000","Premium"));
+                    s.persist(new Client("Fredie","Freda","Makko","password","hungary","AAAAA","31042000","Premium"));
                     t.commit();
                 } else {
                     for(Client c:s.createQuery("from Client", Client.class).list()){
@@ -89,7 +89,7 @@ public class Main {
                 if (s.createQuery("from Account", Account.class).stream().count()<1){
                     Transaction t = s.beginTransaction();
                     MyLogger.getInstance().log("***psinit","db is empty so put some bootstrap data into it. ");
-                    s.persist(new Account("Euro",false,0.00, s.get(Client.class,1)));
+                    s.persist(new Account("Euro",false,100000000000.00, s.get(Client.class,1)));
                     t.commit();
                 } else {
                     for(Account a:s.createQuery("from Account", Account.class).list()){
--- PersistentServer/src/main/java/org/via/sep3/persistentserver/model/Client.java
+++ PersistentServer/src/main/java/org/via/sep3/persistentserver/model/Client.java
@@ -11,17 +11,23 @@ public class Client {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String password;
     private String country;
     private String identityDocument;
     private String birthday;
     private String planType;
 
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL,orphanRemoval = true)
-    private List<Account> account = new ArrayList<>();
+    private List<Account> accounts = new ArrayList<>();
 
-    public Client(String name, String country, String identityDocument, String birthday, String planType) {
-        this.name = name;
+    public Client(String userName,String firstName, String lastName, String password, String country, String identityDocument, String birthday, String planType) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
         this.country = country;
         this.identityDocument = identityDocument;
         this.birthday = birthday;
@@ -35,14 +41,37 @@ public class Client {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public String getCountry() {
         return country;
     }
@@ -76,24 +105,30 @@ public class Client {
     }
 
     public List<Account> getAccounts() {
-        return account;
+        return accounts;
     }
 
     public org.via.sep3.persistentserver.proto.Client getProtoClient(){
         return org.via.sep3.persistentserver.proto.Client.newBuilder()
                 .setClientId(getId())
-                .setName(getName())
+                .setUserName(getUserName())
+                .setFirstName(getFirstName())
+                .setLastName(getLastName())
+                .setPassword(getPassword())
                 .setCountry(getCountry())
                 .setBirthday(getBirthday())
                 .setIdentityDocument(getIdentityDocument())
                 .setPlanType(getPlanType())
                 .build();
     }
+
     @Override
     public String toString() {
         return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+                "userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
                 ", country='" + country + '\'' +
                 ", identityDocument='" + identityDocument + '\'' +
                 ", birthday='" + birthday + '\'' +
