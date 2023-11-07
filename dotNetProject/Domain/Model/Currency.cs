7adea67197129d35649c87b44c60b2abc362e051
Redver <116087243+Redver@users.noreply.github.com>
2023-11-07 13:39:27 +0100

    adding currency

--- dotNetProject/Domain/Model/Currency.cs
+++ dotNetProject/Domain/Model/Currency.cs
@@ -2,5 +2,8 @@ namespace Domain.Model;
 
 public class Currency
 {
-    
+    public string name { get; set; }
+    public double balance { get; set; }
+    public long id { get; set; }
+    public long acountId { get; set; }
 }
