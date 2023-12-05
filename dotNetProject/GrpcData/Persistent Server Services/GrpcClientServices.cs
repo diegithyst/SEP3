68769ec6606f21c44ffe578e0cc48043ffcae301
Maria B <331289@viauc.dk>
2023-12-05 14:05:03 +0100

    Getting password now

--- dotNetProject/GrpcData/Persistent Server Services/GrpcClientServices.cs
+++ dotNetProject/GrpcData/Persistent Server Services/GrpcClientServices.cs
@@ -31,7 +31,7 @@ public class GrpcClientServices : IGrpcClientServices
         }
         else
         {
-            return Task.FromResult(new Client { id = grpcClient.ClientId, firstname = grpcClient.FirstName,lastname = grpcClient.LastName,username = grpcClient.UserName, country = grpcClient.Country, birthday = grpcClient.Birthday, identityDocument = grpcClient.IdentityDocument, planType = newPlan });
+            return Task.FromResult(new Client { id = grpcClient.ClientId, firstname = grpcClient.FirstName,lastname = grpcClient.LastName,username = grpcClient.UserName, password = grpcClient.Password, country = grpcClient.Country, birthday = grpcClient.Birthday, identityDocument = grpcClient.IdentityDocument, planType = newPlan });
 
         }
     }
