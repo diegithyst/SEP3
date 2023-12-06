a7a26f424398f9cde93dbc5371dc555cfcd0d89a
Redver <116087243+Redver@users.noreply.github.com>
2023-12-06 19:32:26 +0100

    added method to services

--- dotNetProject/GrpcData/Persistent Server Services/GrpcAdministratorServices.cs
+++ dotNetProject/GrpcData/Persistent Server Services/GrpcAdministratorServices.cs
@@ -1,10 +1,21 @@
+using System.Security.Cryptography;
 using Application.DaoInterfaces;
 using Domain.Model;
+using PersistentServerClient;
+using Client = Domain.Model.Client;
 
 namespace FileData.DAOs;
 
-public class GrpcAdministratorServices : IAdministratorDao
+public class GrpcAdministratorServices : IAdministratorServices
+
 {
+    private readonly PersistentServerClient.PersistentServer.PersistentServerClient psc;
+
+    public GrpcAdministratorServices(PersistentServer.PersistentServerClient psc)
+    {
+        this.psc = psc;
+    }
+
     public Task<Administrator> CreateAsync(Administrator administrator)
     {
         throw new NotImplementedException();
@@ -12,7 +23,16 @@ public class GrpcAdministratorServices : IAdministratorDao
 
     public Task<Administrator?> GetByUsernameAsync(string username)
     {
-        throw new NotImplementedException();
+        PersistentServerClient.Admin grpcAdmin = psc.GetAdministratorByUsername(new PersistentServerClient.AdministratorBasicDTO() { Username = username,Password = password,EmailDomain = emaildomain });
+        if (grpcAdmin == null)
+        {
+            return null;
+        }
+        else
+        {
+            return Task.FromResult(new Administrator { username = grpcAdmin.username,emailDomain = grpcAdmin.emailDomain,password = grpcAdmin.password});
+
+        }
     }
 
     public Task<Administrator?> GetByIdAsync(long id)
